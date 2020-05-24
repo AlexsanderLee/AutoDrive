9145cd8763ec9c1886bbd7ebf3680131396c159a
Lee <111@qq.com>
2020-05-24 23:07:37 +0800

    add utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+ def computTFIDF(R,iteraion = 5000,alpha=0.0002,lamda = 0.0002):
+     M = len(R)
+     N = len(R[0])
+
+     cost = 0
+     for step in iteration:
+         for user in M:
+             for item in N:
+                 if R[user][item] > 0:
+                    pass
